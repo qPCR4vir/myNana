c275a44950a0972253e8bf0c070ba598d2281e4f
qPCR4vir <arielvina@yahoo.es>
2014-12-03 09:53:51 +0100

    doxy: some fast undocumented thing to the index

--- docs/qPCR4vir.doxygen/Doxigen-source/00-Main.h
+++ docs/qPCR4vir.doxygen/Doxigen-source/00-Main.h
@@ -7,8 +7,8 @@ This is a reference documentation automaticaly generated from the source code of
 ![by Jinhao](nana.jpg)
 
  
-+ [Nana C++ Library - An Open Source C++ Project](http://nanapro.org/)
-+ [Donwload - Sourceforge](https://sourceforge.net/projects/nanapro/), [Git repository - CodePlex](https://nanapro.codeplex.com/)
++ [Nana C++ Library - An Open Source C++ Project](http://nanapro.org/) [Discussion: ideas, questions, bugs](http://nanapro.org/en-us/discuss.htm)
++ [Donwload & tickets - Sourceforge](https://sourceforge.net/projects/nanapro/):, [Git repository & discussions - CodePlex](https://nanapro.codeplex.com/), [mirror](https://github.com/cnjinhao/nana), [2](https://github.com/kirbyfan64/nana)
 + [Nana C++ Library's documentation](http://nanapro.org/help/html/index.html)
 + Blogs:[ - Sourceforge](https://sourceforge.net/p/nanapro/blog/), [Nana's Mill - WordPress](https://nanaproject.wordpress.com/)
 + [User reference documentation](../../Nana.Cpp11.User.Doxygen/html/index.html), [Developers reference documentation](../../Nana.Cpp11.Doxygen/html/index.html)
@@ -18,7 +18,9 @@ This is a reference documentation automaticaly generated from the source code of
 \link nana::checkbox checkbox\endlink,  
 \link nana::combox combox\endlink, 
 \link nana::date_chooser date_chooser\endlink,  
+\link nana::appearance (appearance)\endlink
 \link nana::form form\endlink,  
+\link nana::nested_form nested_form\endlink,  
 \link nana::frame frame\endlink,  
 \link nana::label label\endlink,  
 \link nana::listbox listbox\endlink,  
@@ -37,6 +39,11 @@ This is a reference documentation automaticaly generated from the source code of
 
 + \link nana::paint Paint\endlink: 
 \link nana::animation animation\endlink,  
+\link nana::color color\endlink: (
+\link nana::color_t color_t\endlink,  
+\link nana::color::schema schema\endlink,  
+\link nana::pixel_rgb_t pixel_rgb_t\endlink),  
+\link nana::paint::font font\endlink,  
 \link nana::paint::graphics graphics\endlink,  
 \link nana::drawing drawing\endlink,  
 \link nana::paint::image image\endlink,  
@@ -49,15 +56,24 @@ This is a reference documentation automaticaly generated from the source code of
 \link nana::system::dataexch dataexch\endlink,  
 \link nana::dragger dragger\endlink,  
 \link event events\endlink,  
+\link nana::general_events general events\endlink,  
+\link nana::detail::event_arg_interface events arguments\endlink,  
 \link nana::filebox filebox\endlink,  
 \link nana::filesystem filesystem\endlink,  
+\link nana::internationalization internationalization\endlink,  
+\link nana::keyboard keyboard\endlink,  
 \link nana::login login\endlink,  
+\link nana::notifications notifications\endlink,  
 \link nana::msgbox msgbox\endlink,  
 \link nana::nested_form nested_form\endlink,  
 \link nana::place place\endlink,  
-\link nana::audio::player player (audio)\endlink,  
+\link nana::audio::player audio player\endlink,  
 \link nana::radio_group radio_group\endlink,  
 \link nana::rectangle rectangle\endlink,  
+\link nana::system system:\endlink  
+\link nana::system::sleep (sleep\endlink,  
+\link nana::system::dataexch  dataexch - clipboard\endlink,  
+\link nana::system::open_url open_url\endlink),  
 \link nana::timer timer\endlink,  
 \link nana::threads threads\endlink,  
 \link nana::tooltip tooltip\endlink
